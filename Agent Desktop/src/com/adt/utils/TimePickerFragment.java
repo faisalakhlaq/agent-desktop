2ef134e056587341b9afcaf1f6c54b889ff8fbc5
sardarfaisalpasha <sardarfaisalpasha@gmail.com>
2015-03-14 14:11:31 +0000

    Adding hours functionality fully supported now

--- Agent Desktop/src/com/adt/utils/TimePickerFragment.java
+++ Agent Desktop/src/com/adt/utils/TimePickerFragment.java
@@ -2,6 +2,8 @@ package com.adt.utils;
 
 import java.util.Calendar;
 
+import com.adt.app.R;
+
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
@@ -13,11 +15,14 @@ import android.widget.TimePicker;
 
 public class TimePickerFragment extends DialogFragment implements OnTimeSetListener
 {
+	private Notifier notifier = null;
+
 	private Button timeBtn;
 
-	public TimePickerFragment(Button timeBtn)
+	public TimePickerFragment(Button timeBtn, Notifier notifier)
 	{
 		this.timeBtn = timeBtn;
+		this.notifier = notifier;
 	}
 
 	@Override
@@ -25,17 +30,33 @@ public class TimePickerFragment extends DialogFragment implements OnTimeSetListe
 	{
 		// Use the current time as the default values for the picker
 		final Calendar c = Calendar.getInstance();
-		int hour = c.get(Calendar.HOUR_OF_DAY);
-		int minute = c.get(Calendar.MINUTE);
 
 		// Create a new instance of TimePickerDialog and return it
-		return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+		return new TimePickerDialog(getActivity(), this, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), DateFormat.is24HourFormat(getActivity()));
 	}
 
 	@Override
 	public void onTimeSet(TimePicker view, int hourOfDay, int minute)
 	{
 		timeBtn.setText(hourOfDay + ":" + minute);
-	}
 
+		int btnId = timeBtn.getId();
+
+		switch (btnId)
+		{
+		case R.id.ah_fromtime_btn:
+		{
+			notifier.notify(hourOfDay, "FROMHOUR");
+			notifier.notify(minute, "FROMMINUTE");
+			break;
+		}
+		case R.id.ah_totime_btn:
+		{
+			notifier.notify(hourOfDay, "TOHOUR");
+			notifier.notify(minute, "TOMINUTE");
+			break;
+		}
+		}
+
+	}
 }
